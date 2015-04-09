b1d9df4fd90d4c6650cfee811be4a853d9628bc1
Scott Chen <rightson@gmail.com>
2015-04-09 14:12:22 +0800

    add test for reentrant string parser

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main(int argc, char const *argv[])
     mainParser parser;
     yy_file_parser(parser, fopen("input.txt","r"));
     yy_string_parser(parser, "123 + 456 \n\0");
+    yy_string_parser(parser, "123 + 456 + 789 \n\0");
     return 0;
 }
